7c8c8da6e06291dc346125b35942c629bce43500
wells102 <51076931+wells102@users.noreply.github.com>
2019-05-27 14:31:19 +0530

    Create abc.sh

---
+++ abc.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+pwd
+ls -ltr
+touch test
+ls -ltr
+echo "testing" >> test
+ls -ltr
+echo "$Environment for $Variable" >> test
+
+echo "[INFO] GIT Jenkins Integratioon Demo"
+echo "[INFO] POLL SCM Demo"
